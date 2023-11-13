c65a3c54ebb78041f32a83518b505bdbb439f0e6
Hilcbek <balemayehu07@gmail.com>
2023-11-13 21:11:20 +0300

    sdasdsdssdssdssdss

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(
 );
 app.use(express.json())
 app.use(morgan('common'))
-let PORT = process.env.PORT || 5000
+let PORT = process.env.PORT
 let MONGODB = process.env.MONGODB;
 mongoose.connect(MONGODB).then(() => {
     app.listen(PORT,() => console.log(`Server is running on port ${PORT}`))
