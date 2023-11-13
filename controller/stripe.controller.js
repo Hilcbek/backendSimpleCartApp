033f7f4761a88ae474620a487ea94076e65b57a4
Hilcbek <balemayehu07@gmail.com>
2023-11-13 23:02:59 +0300

    adas

--- controller/stripe.controller.js
+++ controller/stripe.controller.js
@@ -1,32 +1,32 @@
-import Stripe from "stripe";
-import expressAsync from "express-async-handler";
-import dotenv from "dotenv";
-dotenv.config();
-let stripe = new Stripe(process.env.Stripe_Secret);
-export let Payment = expressAsync(async (req, res, next) => {
-  let { products } = req.body;
-  let line_items = products.map(product => {
-    return {
-      price_data: {
-        currency: "usd",
-        product_data: {
-          name: product.title,
-          description: product.desc,
-        },
-        unit_amount: product.price * 100,
-      },
-      quantity: product.quantity,
-    };
-  })
-  try {
-    let session = await stripe.checkout.sessions.create({
-      line_items: line_items,
-      mode: "payment",
-      success_url: "http://localhost:5173/success",
-      cancel_url: "http://localhost:5173",
-    });
-    res.send({url : session.url});
-  } catch (error) {
-    next(error);
-  }
-});
+// import Stripe from "stripe";
+// import expressAsync from "express-async-handler";
+// import dotenv from "dotenv";
+// dotenv.config();
+// let stripe = new Stripe(process.env.Stripe_Secret);
+// export let Payment = expressAsync(async (req, res, next) => {
+//   let { products } = req.body;
+//   let line_items = products.map(product => {
+//     return {
+//       price_data: {
+//         currency: "usd",
+//         product_data: {
+//           name: product.title,
+//           description: product.desc,
+//         },
+//         unit_amount: product.price * 100,
+//       },
+//       quantity: product.quantity,
+//     };
+//   })
+//   try {
+//     let session = await stripe.checkout.sessions.create({
+//       line_items: line_items,
+//       mode: "payment",
+//       success_url: "http://localhost:5173/success",
+//       cancel_url: "http://localhost:5173",
+//     });
+//     res.send({url : session.url});
+//   } catch (error) {
+//     next(error);
+//   }
+// });
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import morgan from "morgan";
 import cors from "cors";
 import { routerUser } from "./router/user.router.js";
-import { routerStripe } from "./router/stripe.router.js";
+// import { routerStripe } from "./router/stripe.router.js";
 let app = express();
 dotenv.config();
 app.use(
@@ -23,7 +23,7 @@ mongoose.connect(MONGODB).then(() => {
 mongoose.connection.on('connected',() => console.log('mongodb start listening again'))
 mongoose.connection.on('disconnected',() => console.log('mongodb stopped listening'))
 app.use('/api/auth',routerUser)
-app.use("/api/stripe", routerStripe);
+// app.use("/api/stripe", routerStripe);
 app.use((err,req,res,next) => {
     let errorMessage = err.message
     let errorStatus = err.status
